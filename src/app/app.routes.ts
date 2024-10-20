b1469cb977ad073318acd6c820a6095347073d14
Sudhir Yadav <Sudhir.Yadav@IDeaS.com>
2024-10-20 14:22:35 +0530

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {authGuard} from "./features/core/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    children:[
+      {
+        path: '', pathMatch: 'full', redirectTo: 'home',
+      },
+      {
+        path: 'home', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'weather', loadComponent: () => import('./features/weather/weather-page/weather-page.component')
+          .then(m => m.WeatherPageComponent)
+      }
+    ]
+  },
+];
